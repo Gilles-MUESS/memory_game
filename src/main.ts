2e76ccf81693382e33f1daaed6f29617ced8682a
Gilles-MUESS <gillesmuess.pro@gmail.com>
2025-02-27 19:51:51 +0100

    Ajout des cartes au DOM

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './main.scss';
+
+import Board from './types/board';
+
+const boardEl: HTMLElement | null = document.getElementById('gameBoard');
+const board = new Board(boardEl, 'https://picsum.photos/100');
+const cards = board.getCards();
+
+Promise.all(cards.map((card) => card.ready())).then(() => {
+  cards.forEach((card) => {
+    boardEl?.appendChild(card.createCard());
+  });
+});
