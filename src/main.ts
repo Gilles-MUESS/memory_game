b98d38231b15d914369a24fc5607bed318934ce6
Gilles-MUESS <gillesmuess.pro@gmail.com>
2025-02-27 18:46:48 +0100

    Revert "project init"
    This reverts commit 36f9c21104a95b87b0bbd06c73f32714d4c797ee.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,1 @@
 import './main.scss';
-
-import Card from './types/card';
-
-// Jeu de 16 cartes, 8 cartes de bases
-const nbCards = 16;
-
-for (let i = 0; i < nbCards / 2; i++) {
-  const card = new Card('https://picsum.photos/100');
-  document.body.appendChild(card.createCard());
-  document.body.appendChild(card.createCard());
-}
